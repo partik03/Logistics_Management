eade0e24acc2474ff6e6f2d02fb96a34bea52630
HitanshuPrasad <hitanshuprasad@gmail.com>
2023-10-13 16:26:47 +0530

    Added thymeleaf configs

--- src/main/java/com/logistics/web/repository/CarrierRepository.java
+++ src/main/java/com/logistics/web/repository/CarrierRepository.java
@@ -1,8 +1,8 @@
-package com.logistics.web.repository;
+// package com.logistics.web.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.logistics.web.models.Carrier;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.logistics.web.models.Carrier;
 
-public interface CarrierRepository extends JpaRepository<Carrier,Long > {
+// public interface CarrierRepository extends JpaRepository<Carrier,Long > {
 
-}
+// }
