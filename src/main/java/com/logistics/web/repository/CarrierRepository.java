1f25f4abe2b50acdc1e4190ad0c27b609ea25c53
hs05june <harpreet.singh.cse21@itbhu.ac.in>
2023-10-18 09:41:43 +0530

    Made Complaint model
    Signed-off-by: hs05june <harpreet.singh.cse21@itbhu.ac.in>

--- src/main/java/com/logistics/web/repository/CarrierRepository.java
+++ src/main/java/com/logistics/web/repository/CarrierRepository.java
@@ -1,16 +1,13 @@
-// package com.logistics.web.repository;
-//
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.logistics.web.models.Carrier;
-//
-//import java.util.List;
-//
-//public interface CarrierRepository extends JpaRepository<Carrier,Long > {
-//
-//    List<Carrier> findCarriersByID(int Id);
-//
-//    Carrier findCarrierByID(int Id);
-//
-//
-//
-//}
+package com.logistics.web.repository;
+
+import com.logistics.web.models.Carrier;
+
+@Repository
+public class CarrierRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CarrierRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
